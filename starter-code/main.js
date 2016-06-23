87570ec0280a0222dbae3ee0c6ff47c1aea48bfc
Chris Khoo <chriskhoo@Chriss-MBP.home>
2016-06-23 21:05:55 +0800

    Create if check with alert

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,4 +2,11 @@ console.log("JS file is connected to HTML! Woo!")
 var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
-var cardFour = "king"; 
+var cardFour = "king";
+
+if (cardTwo === cardFour) {
+    alert("You found a match!");
+}
+else {
+    alert("Sorry try again.")
+    };
